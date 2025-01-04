9d98e32331637469308ed7ebbdae3b9d4d6bd14f
Ashutosh Pandey <96343236+pandeyashutosh02@users.noreply.github.com>
2025-01-04 23:39:47 +0530

    Time: 415 ms (31.29%), Space: 16.1 MB (40.49%) - LeetHub

---
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int countPalindromicSubsequence(string s) {
+        unordered_map<char, pair<int, int>> mp;
+
+        for(int i=0;i<s.size();i++) {
+            if(mp.find(s[i]) == mp.end()) {
+                mp[s[i]] = {i, i};
+            }
+            else {
+                mp[s[i]].second = i;
+            }
+        }
+
+        int res=0;
+        for(auto x : mp) {
+            set<char> uniq;
+            char ch = x.first;
+            int st = x.second.first, end = x.second.second;
+            if(st==end || end-st<2)continue;
+            for(int j=st+1;j<end;j++)uniq.insert(s[j]);
+            res += uniq.size();
+        }
+        return res;
+    }
+};
