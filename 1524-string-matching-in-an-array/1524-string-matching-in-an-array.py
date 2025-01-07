bff707e7c841b6dbd210062d3d4e8b60c5f5e1f5
Ashutosh Pandey <96343236+pandeyashutosh02@users.noreply.github.com>
2025-01-07 23:22:03 +0530

    Time: 3 ms (79.46%), Space: 12.4 MB (20.54%) - LeetHub

---
+++ 1524-string-matching-in-an-array/1524-string-matching-in-an-array.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def stringMatching(self, words):
+        """
+        :type words: List[str]
+        :rtype: List[str]
+        """
+        ans=[]
+
+        for i in range(len(words)):
+            for j in range(len(words)):
+                if i==j:
+                    continue
+                if words[i] in words[j]:
+                    ans.append(words[i])
+                    break
+        return ans
+        
