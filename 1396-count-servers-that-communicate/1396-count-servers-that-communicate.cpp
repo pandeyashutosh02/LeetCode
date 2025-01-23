2c619b0f5d1621ca1d51da531d7331fde5623298
Ashutosh Pandey <96343236+pandeyashutosh02@users.noreply.github.com>
2025-01-23 11:54:06 +0530

    Time: 1 ms (64.83%), Space: 26.8 MB (26.25%) - LeetHub

--- 1396-count-servers-that-communicate/1396-count-servers-that-communicate.cpp
+++ 1396-count-servers-that-communicate/1396-count-servers-that-communicate.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
     int countServers(vector<vector<int>>& grid) {
-        unordered_map<int, int> row, col;
-        int total_servers=0, alone=0;
+        int m=grid.size(), n=grid[0].size();
+        vector<int> row(m,0), col(n,0);
+        int res=0;
         for(int i=0; i<grid.size(); i++) {
             for(int j=0; j<grid[0].size(); j++) {
                 if(grid[i][j] == 1) {
-                    total_servers++;
                     row[i]++;
                     col[j]++;
                 }
             }
         }
 
-        set<pair<int, int>> s;
         for(int x=0; x<grid.size(); x++) {
             for(int y=0; y<grid[0].size(); y++) {
-                if(grid[x][y] == 1) {
-                    if(row[x]>=2 || col[y]>=2)s.insert({x,y});
-                }
+                if(grid[x][y] == 1 and (row[x]>=2 || col[y]>=2))res++;
             }
         }
-        return s.size();
+        return res;
     }
 };
