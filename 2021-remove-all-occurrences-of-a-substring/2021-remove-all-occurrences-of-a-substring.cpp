ba0af7d52bd98158f70863b6385898a7948a3192
Ashutosh Pandey <96343236+pandeyashutosh02@users.noreply.github.com>
2025-02-11 13:08:53 +0530

    Time: 3 ms (32.43%), Space: 9.3 MB (90.62%) - LeetHub

---
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    string removeOccurrences(string s, string part) {
+        int n = part.size();
+        while(s.find(part) != string::npos) {
+            s.erase(s.find(part), n); 
+        }
+        return s;
+    }
+};
