4b3c3f1331a3c9fdf55be9c3ae356dc5e2cc3fd4
Ashutosh Pandey <96343236+pandeyashutosh02@users.noreply.github.com>
2025-02-23 00:25:37 +0530

    Time: 5517 ms (5.04%), Space: 14.1 MB (33.97%) - LeetHub

---
+++ 0136-single-number/0136-single-number.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        for i in range(len(nums)):
+            if nums.count(nums[i])==1:
+                return nums[i]
+
+        return 0
